c56a74530a1486075b4b8391ea9d387e86145417
Sadegh Sadeghi Tabas <31417680+SadeghTabas-NOAA@users.noreply.github.com>
2023-10-19 21:29:22 -0400

    args

--- preprocessing.py
+++ preprocessing.py
@@ -21,3 +21,16 @@ class DataProcessor:
 
 
     def era5_process(self):
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download and process GFS and ERA5 data")
+    parser.add_argument("start_date", help="Start date in the format 'YYYYMMDD'")
+    parser.add_argument("end_date", help="End date in the format 'YYYYMMDD'")
+    parser.add_argument("-o", "--output", help="Output directory for processed data")
+    parser.add_argument("-d", "--download", help="Download directory for raw data")
+    parser.add_argument("-k", "--keep", help="Keep downloaded data (optional)", action="store_true", default=False)
+    parser.add_argument("-p", "--process", nargs="*", choices=["gfs", "era5"], help="Specify which process to run")
+    args = parser.parse_args()
