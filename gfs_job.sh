026ebbcaacb620a98d65677ef5a8178d30c90f32
Sadegh Sadeghi Tabas <31417680+SadeghTabas-NOAA@users.noreply.github.com>
2023-10-20 10:49:07 -0400

    Create gfs_job.sh

---
+++ gfs_job.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH -J gfs_process
+#SBATCH -o /contrib/Sadegh.Tabas/ML4BC/gfs_output.out
+#SBATCH -e /contrib/Sadegh.Tabas/ML4BC/gfs_output.out
+#SBATCH -N 1
+#SBATCH -t 30-00:00:00  # 1 month (30 days)
+
+# Load Miniconda module
+source ~/.bashrc
+
+# Activate the Conda environment
+conda activate ml4bc
+
+# Navigate to the working directory
+cd /lustre/ML4BC
+
+# Run the Python script
+python3 preprocessing.py 20210323 20220101 -p gfs -o /contrib/Sadegh.Tabas/ML4BC/
+
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+"gfs_job.sh" 19L, 459C                                                                                                                                                                                     
