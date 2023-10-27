acbdbd9db6687050166b55c0bc7a63d7f54abf55
Sadegh Sadeghi Tabas <sadeghst@SadeghST-2.local>
2023-10-26 22:38:45 -0400

    parallel

--- .ipynb_checkpoints/ml4bc-checkpoint.py
+++ .ipynb_checkpoints/ml4bc-checkpoint.py
@@ -16,16 +16,16 @@ class Autoencoder(nn.Module):
 
         # Encoder
         self.encoder = nn.Sequential(
-            nn.Conv3d(1, 64, kernel_size=(3,3,3), padding=(1,1,1)),
+            nn.Conv3d(1, 32, kernel_size=(3, 3, 3), padding=(1, 1, 1)),
             nn.ReLU(True),
-            #nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+            # nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
         )
-        
+
         # Bottleneck (no further reduction of dimensions)
 
         # Decoder
         self.decoder = nn.Sequential(
-            nn.ConvTranspose3d(64, 1, kernel_size=(3,3,3), padding=(1,1,1)),
+            nn.ConvTranspose3d(32, 1, kernel_size=(3, 3, 3), padding=(1, 1, 1)),
             nn.Sigmoid()
         )
 
@@ -34,13 +34,12 @@ class Autoencoder(nn.Module):
         decoded = self.decoder(encoded)
         return decoded
 
-# Create autoencoder model
-autoencoder = Autoencoder().to(device)
-
+# Create autoencoder model and use all available GPUs
 if torch.cuda.device_count() > 1:
     print("Using", torch.cuda.device_count(), "GPUs!")
-    autoencoder = nn.DataParallel(autoencoder)
-
+    autoencoder = nn.DataParallel(Autoencoder()).to(device)
+else:
+    autoencoder = Autoencoder().to(device)
 
 class NetCDFDataset(Dataset):
     def __init__(self, root_dir, transform=None):
@@ -52,24 +51,25 @@ class NetCDFDataset(Dataset):
 
     def __getitem__(self, idx):
         file_path = os.path.join(self.root_dir, self.file_list[idx])
-        
+
         # Load NetCDF data
         dataset = nc.Dataset(file_path)
-        data = dataset.variables['t2m'][:].astype(np.float32)  # Adjust 'data' to the variable name in your file
+        data = dataset.variables['t2m'][:].astype(np.float32)
         dataset.close()
-        
+
         # Reshape the data to (1, 50, 721, 1440)
         data = data.reshape(1, 50, 721, 1440)
         return torch.tensor(data)
+
 
 # Define your data directories
 gfs_biased_dir = 'GFS/'
 era5_unbiased_dir = 'ERA5/'
 
-batch_size = 2  # Adjust batch size as needed
+batch_size = 2
 shuffle = False
-num_workers = 0  # Adjust the number of workers for data loading
-seed = 42  # Your desired seed value
+num_workers = 0
+seed = 42
 torch.manual_seed(seed)
 
 # Define the loss function and optimizer
@@ -83,26 +83,29 @@ era5_dataset = NetCDFDataset(root_dir=era5_unbiased_dir)
 gfs_data_loader = DataLoader(gfs_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 era5_data_loader = DataLoader(era5_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
 
-# Training loop with tqdm
+
+# Training loop with a custom progress bar
 num_epochs = 10
 for epoch in range(num_epochs):
     autoencoder.train()
     total_loss = 0.0
-    
-    # Wrap the DataLoader with tqdm for a progress bar
-    for gfs_data, era5_data in tqdm(zip(gfs_data_loader, era5_data_loader), desc=f'Epoch [{epoch+1}/{num_epochs}]'):
+
+    # Create a custom progress bar for the epoch
+    progress_bar = tqdm(enumerate(zip(gfs_data_loader, era5_data_loader)), total=len(gfs_data_loader), desc=f'Epoch [{epoch+1}/{num_epochs}]', dynamic_ncols=True)
+    for batch_idx, (gfs_data, era5_data) in progress_bar:
         optimizer.zero_grad()
         outputs = autoencoder(gfs_data.to(device))
         loss = criterion(outputs, era5_data.to(device))
         loss.backward()
         optimizer.step()
-        
+
         total_loss += loss.item()
-    
+
+    progress_bar.close()  # Close the custom progress bar
+
     # Calculate and print the average loss for the epoch
     avg_loss = total_loss / len(gfs_data_loader)
     print(f'Epoch [{epoch+1}/{num_epochs}], Average Loss: {avg_loss:.4f}')
 
 # Save the trained model
 torch.save(autoencoder.module.state_dict() if isinstance(autoencoder, nn.DataParallel) else autoencoder.state_dict(), 'autoencoder_model.pth')
-
